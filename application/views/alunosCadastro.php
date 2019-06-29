ab559a52d85f0aa2de9f7cafdfe69ab80d58a79d
26rafael26 <>
2019-06-28 21:48:31 -0300

    model e view do projeto

---
+++ application/views/alunosCadastro.php
@@ -0,0 +1,32 @@
+<div class = "container">
+    <div class= "row">
+        <div class= "col-lg-3 col-md-3 col-3"></div>
+        <div class= "col-lg-6 col-md-6 col-6">
+
+
+            <form method="post" style="text-align: center;" class=
+                  "form-group">
+                <form method="post" class="form-group">
+                    <div class="form-group">
+                        <input name = "emp_id" class="form-control"  type = "text" id = "emp_id" size="3" maxlength="2" placeholder="    ID">
+                    </div>
+                    <input name = "delete" type = "submit" id = "delete" value = "Deletar" class="btn btn-danger"> <p>
+
+                    <div class="form-group">
+                        <input name="up_id" class="form-control" type="text" id = "up_id" size="2" maxlength="2" placeholder="    ID">
+                    </div>
+                    <input name = "update" type = "submit" id = "update" value = "Atualizar" class="btn btn-success"> <br><br><br>
+
+
+                    <div class="form-group">
+                        <input type="text" name="nome" placeholder="Digite o nome ..." class="form-control">
+                    </div>
+                    <input name="pesquisar" type="submit" value="Pesquisar" class="btn btn-primary"><br><br>
+
+                    <input name="todos" type="submit" value="Listar Todos" class="btn btn-warning"><br><br>
+
+                </form>
+			</div>
+			<div class= "col-lg-6 col-md-6 col-6"></div>
+	    </div>
+
